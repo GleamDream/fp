417c30137f7e74d7dfbb631a4a64ca64b8101e0a
HiromuIkemura <ac194601@edu.okinawa-ct.ac.jp>
2021-07-05 23:33:10 +0900

    add Map4Color

---
+++ Map4Color/2150001.py
@@ -0,0 +1,183 @@
+# name: Hiromu Ikemura
+# id: 2150001
+
+from PIL import Image
+import sys
+
+def product(x, y):
+    ret = list()
+    for _x in x:
+        for _y in y:
+            ret.append((_x, _y))
+    return ret
+
+class UnionFind:
+    def __init__(self, n: int, m: int):
+        self.table = [[(-1, -1)] * n for _ in range(m)]
+    
+    def root(self, p: tuple) -> tuple:
+        x, y = p
+        stack = list()
+        tbl = self.table
+        while tbl[y][x] >= (0, 0):
+            stack.append(p)
+            x, y = tbl[y][x]
+        for _x, _y in stack:
+            tbl[_y][_x] = (x, y)
+        return (x, y)
+    
+    def find(self, p1: tuple, p2: tuple) -> bool:
+        return self.root(p1) == self.root(p2)
+    
+    def union(self, p1: tuple, p2: tuple) -> None:
+        r1 = self.root(p1)
+        r2 = self.root(p2)
+        if r1 == r2:
+            return 
+        
+        d1 = self.table[r1[1]][r1[0]]
+        d2 = self.table[r2[1]][r2[0]]
+        if d1 <= d2:
+            self.table[r2[1]][r2[0]] = r1
+            if d1 == d2:
+                self.table[r1[1]][r1[0]] = (self.table[r1[1]][r1[0]][0] - 1, self.table[r1[1]][r1[0]][1] - 1)
+        else:
+            self.table[r1[1]][r1[0]] = r2
+
+class ImageProcessing:
+    def __init__(self, img: Image):
+        self.img = img
+        self.w, self.h = self.img.size
+        self.uf = UnionFind(self.w, self.h)
+        self.dgraph = dict()
+        self.frame = set()
+        self.fw = 5 # Frame Wdith
+        self.color = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255)]
+    
+    def classify(self) -> None:
+        for y, x in product(range(self.h - 1), range(self.w - 1)):
+            if self.img.getpixel((x, y)) == (0, 0, 0):
+                self.frame.add((x, y))
+                continue
+            for _x, _y in zip([x, x + 1], [y + 1, y]):
+                if self.img.getpixel((x, y)) == self.img.getpixel((_x, _y)):
+                    self.uf.union((x, y), (_x, _y))
+    
+    def paint(self, colors) -> None:
+        new = Image.new("RGB", (self.w, self.h))
+        for y, x in product(range(self.h - 1), range(self.w - 1)):
+            if (x, y) in self.frame:
+                new.putpixel((x, y), (0, 0, 0))
+                continue
+            new.putpixel((x, y),
+                self.color[colors.get(self.uf.root((x, y)), 0)]
+            )
+        new.save("a.png")
+    
+    def graph(self):
+        for y, x in product(range(self.h - self.fw), range(self.w - self.fw)):
+            if (x, y) in self.frame or (x, y + self.fw) in self.frame or (x + self.fw, y) in self.frame:
+                continue
+            if not self.uf.find((x, y), (x, y + self.fw)):
+                _x, _y = self.uf.root((x, y))
+                self.dgraph.setdefault((_x, _y), set())
+                self.dgraph[(_x, _y)].add(self.uf.root((x, y + self.fw)))
+
+            if not self.uf.find((x, y), (x + self.fw, y)):
+                _x, _y = self.uf.root((x, y))
+                self.dgraph.setdefault((_x, _y), set())
+                self.dgraph[(_x, _y)].add(self.uf.root((x + self.fw, y)))
+        
+        points: set = set()
+        for key, value in self.dgraph.items():
+            points.add(key)
+            for v in value:
+                points.add(v)
+        
+        for p in points:
+            self.dgraph.setdefault(p, set())
+            for key, value in self.dgraph.items():
+                if key == p:
+                    continue
+                if p in value:
+                    self.dgraph[p].add(key)
+
+class Queue:
+    def __init__(self, data = []):
+        self.data = data
+
+    def append(self, x):
+        self.data.append(x)
+        return self.data
+    
+    def popleft(self):
+        cell = self.data[0]
+        del self.data[0]
+        return cell
+
+class Node:
+    def __init__(self, point: tuple, nears: list):
+        self.point: tuple = point
+        self.nears: list = nears
+        self.color: int = 0
+    
+    def __repr__(self) -> str:
+        return f'Node index{self.point} nears:{self.nears} sign:{self.color}'
+
+class Solver:
+    def __init__(self, graph: dict):
+        self.graph: dict = graph
+        self.nodes: dict = dict()
+        self.nc: int = 4
+        self.retry = 0
+        for key, value in graph.items():
+            self.nodes[key] = Node(key, value)
+    
+    def score(self) -> int:
+        s: int = 0
+        for key, value in self.graph.items():
+            for v in value:
+                if self.nodes[key].color == self.nodes[v].color:
+                    s -= 1
+        return s
+
+    def solve(self) -> dict:
+        queue: Queue = Queue()
+        for k in self.nodes.keys():
+            queue.append(self.nodes[k])
+        
+        before: int = -1e8
+        after: int = -1e8
+        while queue.data and after != 0:
+            node: Node = queue.popleft()
+            befcolor: int = node.color
+            before = self.score()
+            for i in range(3):
+                self.nodes[node.point].color = (self.nodes[node.point].color + 1) % self.nc
+                after = self.score()
+                if before <= after:
+                    break
+            if before <= after:
+                for near in node.nears:
+                    queue.append(self.nodes[near])
+            else:
+                self.nodes[node.point].color = befcolor
+        
+        return {v.point: v.color for v in self.nodes.values()}
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("You will need to execute with filename")
+        sys.exit(-1)
+    print("Image Open")
+    ip: ImageProcessing = ImageProcessing(Image.open(sys.argv[1]).convert("RGB"))
+    print("Map Classify")
+    ip.classify()
+    print("Generate Graph")
+    ip.graph()
+    solver: Solver = Solver(ip.dgraph)
+    print("Solve")
+    ans: dict = solver.solve()
+    print("Paint")
+    ip.paint(ans)
+    print("Done")
